5a1fe1a0b2bb970a03e89c6b38df0cb72f76aef7
Shrisha Yapalparvi <shrishayap@gmail.com>
2023-02-13 21:02:39 -0500

    streaming data from vid to collector

--- src/app.py
+++ src/app.py
@@ -1,5 +1,11 @@
 import eel
+import socket
+import subprocess
+import pickle
 
+#defines
+video_port = 12345
+video_stream_size = 4096
 
 eel.init('web')
 
@@ -30,17 +36,39 @@ def sendData(subject, trial, task, rate, range_val, hemisphere, filter_val):
         "Filter": filter_val
         }
     print(data)
-    getInputStreams(data)
+    path = "FILL OUT" #TODO
+    getInputStreams(path)
     return
 
-def getInputStreams(data: dict):
+def getInputStreams(path: str):
     '''
-    Uses data to create dynamic directory, and reach out to different input streams. 
+    Reaches out to different data streams (socket) to get data. Prepares to write them to file 
 
-    data: Dictionary that contains keys Subject, Trial, Task, Rate, Range, Hemisphere, and Filter.
+    path: string that shows path where data should be stored
 
     returns: Nothing
     '''
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as video:
+        video.bind(('localhost', video_port))
+        video.listen()
+        
+
+        vid_con, _ = video.accept()
+        print("Connected to Video")
+
+        while True:
+
+            #video data processing
+            vid_data = pickle.loads(vid_con.recv(video_stream_size))
+            if vid_data == ["Data not collected", "N/A"]: #something wrong with video data
+                pass #update later with what to do
+
+
+            if not vid_data:
+                pass
+            print(vid_data)
+                
     return
 
 eel.start('index.html')

3eb71268aafaa4edbf7a5ac37fc9d4b6914bb912
Shrisha Yapalparvi <shrishayap@gmail.com>
2023-04-06 22:02:52 -0400

    unified js

--- src/web/main.js
+++ src/web/main.js
@@ -3,14 +3,11 @@ function sendData() {
     var trial = document.getElementById("trial").value;
     var task = document.getElementById("task").value;
     var rate = document.getElementById("rate").value;
-    var range_val = document.getElementById("range_val").value;
-    var hemisphere = document.getElementById("hemisphere").value;
-    var filter_val = document.getElementById("filter_val").value;
 
     if (!subject || !trial || !task || !rate) {
         alert("Please fill out all forms");
         return false;
       } else {
-        eel.sendData(subject, trial, task, rate, range_val, hemisphere, filter_val)();
+        eel.sendData(subject, trial, task, rate)();
       }
 }

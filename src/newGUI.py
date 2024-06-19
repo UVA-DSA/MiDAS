65cd0e7eeae8112b3dc47d3385d94c59a9759a7e
zschrader <zgr3jk@virginia.edu>
2024-06-19 14:47:00 -0400

    Started diagnostic connection code and pedal code

--- src/newGUI.py
+++ src/newGUI.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import font
 from tkinter import *
 from PIL import ImageTk,Image
+import queue
 
 win = tk.Tk()
 # Vars for the info of each trial
@@ -10,13 +11,14 @@ trial_var = tk.StringVar()
 task_var = tk.StringVar()
 rate_var = tk.StringVar()
 
-def startGui():
+def startGui(capture_q, trackstar_q, PDS_q):
+    global pUL, pUR, pLL, pLR, pCam, pClutch, pLong
+    global capOneInd, capTwoInd, camOneInd, camTwoInd, arduinoInd, trackstarInd, watchLeftInd, watchRightInd
     # Window and the 3 Frames creation
     win.title("MIDAS V3 - Data Collection System")
     # win.geometry("1800x1200")
     # set minimum window size value
     win.minsize(1080, 300)
-    
     # set maximum window size value
     win.maxsize(1920, 1080)
 
@@ -44,18 +46,28 @@ def startGui():
     tk.Button(infoFr, text='Submit', command=submitData, font=large_font, width=5).grid(column=0, row=4, columnspan=2, pady=10)
 
     # Pedal Indicators Frame
+    global pedalFr
     pedalFr = tk.LabelFrame(win, text="Pedal Indicators", padx=15, pady=15, font=large_font_bold)
     pedalFr.grid(column=0, row=1, padx=10, pady=10, sticky="nsew", columnspan=2)
 
+    pUL = tk.Label(pedalFr)
+    pUR = tk.Label(pedalFr)
+    pLL = tk.Label(pedalFr)
+    pLR = tk.Label(pedalFr)
+    pCam= tk.Label(pedalFr)
+    pClutch = tk.Label(pedalFr)
+    pLong = tk.Label(pedalFr)
+
     pedals = [
-        ("Upper Left", 0, 2, 1), ("Upper Right", 0, 3, 1),
-        ("Lower Left", 1, 2, 1), ("Lower Right", 1, 3, 1),
-        ("Clutch", 0, 1, 1), ("Camera", 1, 1, 1), ("Long", 0, 0, 3)
+        ("Upper Left", 0, 2, 1, pUL), ("Upper Right", 0, 3, 1, pUR),
+        ("Lower Left", 1, 2, 1, pLL), ("Lower Right", 1, 3, 1, pLR),
+        ("Clutch", 0, 1, 1, pClutch), ("Camera", 1, 1, 1, pCam), ("Long", 0, 0, 3, pLong)
     ]
 
     for pedal in pedals:
-        text, row, col, rowspan = pedal
-        tk.Label(pedalFr, text=text, bg="red", width=9, height=4, font=large_font).grid(row=row, column=col, padx=10, pady=10, rowspan=rowspan)
+        text, row, col, rowspan, pedalName = pedal
+        pedalName.config(text=text, bg="red", width=9, height=4, font=large_font)
+        pedalName.grid(row=row, column=col, padx=10, pady=10, rowspan=rowspan)
 
     # Capture Frame
     captureFr = tk.LabelFrame(win, text="Captures", padx=15, pady=15, font=large_font_bold)
@@ -66,29 +78,93 @@ def startGui():
     image = ImageTk.PhotoImage(Image.open("test/test.jpg"))
     capOne.create_image(108,72, anchor="center", image = image)
 
-
     # Diagonstic Frame
+    global diagnosticFr
     diagnosticFr = tk.LabelFrame(win, text="Diagnostics", padx=15, pady=15, font=large_font_bold)
     diagnosticFr.grid(column=1, row=0, rowspan=1, padx=10, pady=10, sticky="nsew")
+    capOneInd = tk.Label(diagnosticFr)
+    capTwoInd = tk.Label(diagnosticFr)
+    watchLeftInd = tk.Label(diagnosticFr)
+    watchRightInd = tk.Label(diagnosticFr)
+    arduinoInd = tk.Label(diagnosticFr)
+    trackstarInd = tk.Label(diagnosticFr)
+    camOneInd = tk.Label(diagnosticFr)
+    camTwoInd = tk.Label(diagnosticFr)
     devices = [
-        ("Capture 1:", "Error"), ("Capture 2:", "Error"),
-        ("Watch Left:", "Error"), ("Watch Right:", "Error"),
-        ("Arduino (Pedals):", "Error"), ("Trackstar:", "Error"),
-        ("Camera 1:", "Error"), ("Camera 2:", "Error")
-    ]
+        ("Capture 1:", capOneInd), ("Capture 2:",  capTwoInd),
+        ("Watch Left:", watchLeftInd), ("Watch Right:",  watchRightInd),
+        ("Arduino (Pedals):", arduinoInd), ("Trackstar:",  trackstarInd),
+        ("Camera 1:",  camOneInd), ("Camera 2:", camTwoInd)
+    ]   
 
-    for i, (label, status) in enumerate(devices):
+    for i, (label, name) in enumerate(devices):
         tk.Label(diagnosticFr, text=label, font=large_font).grid(row=i, column=0, padx=10, pady=5, sticky="w")
-        tk.Label(diagnosticFr, text=status, bg="red", width=8, font=large_font).grid(row=i, column=1, padx=10, pady=5, sticky="w")
-
+        name.config(bg="yellow", width=8, font=large_font)
+        name.grid(row=i, column=1, padx=10, pady=5, sticky="w")
+    
     # Configure weight for responsiveness
     for i in range(2):
         win.grid_columnconfigure(i, weight=1)
     for i in range(3):
         win.grid_rowconfigure(i, weight=1)
 
+    #diagonosticUpdates(capture_q, trackstar_q, PDS_q)
+    pedalUpdates(PDS_q)
     win.mainloop()
 
 def submitData():
     from app import sendData
     sendData(subject_var.get(), trial_var.get(), task_var.get(), rate_var.get())
+
+def diagonosticUpdates(capture_q, trackstar_q, PDS_q):
+    if capture_q.empty() == True:
+        capOneInd.config(bg = "red", text="Error")
+        capTwoInd.config(bg = "red", text="Error")
+    else:
+        capOneInd.config(bg = "green", text="Good")
+        capTwoInd.config(bg = "red", text="Error")
+    if trackstar_q.empty() == True:
+        trackstarInd.config(bg = "red", text="Error")
+    else:
+        trackstarInd.config(bg = "green", text="Good")
+    diagnosticFr.after(1000, diagonosticUpdates, capture_q,trackstar_q,PDS_q)
+
+def pedalUpdates(PDS_q):
+    try:
+        out = PDS_q.get(True, 0.5)
+        arduinoInd.config(bg = "green", text="Good")
+    except queue.Empty:
+        arduinoInd.config(bg = "red", text="Error")
+    else:
+        print(out)
+        if '1' in out:
+            pUL.config(bg="green")
+        else:
+            pUL.config(bg="red")
+        if '2' in out:
+            pUR.config(bg="green")
+        else:
+            pUR.config(bg="red")
+        if '3' in out:
+            pLL.config(bg="green")
+        else:
+            pLL.config(bg="red")
+        if '4' in out:
+            pLR.config(bg="green")
+        else:
+            pLR.config(bg="red")
+        if '5' in out:
+            pClutch.config(bg="green")
+        else:
+            pClutch.config(bg="red")
+        if '6' in out:
+            pCam.config(bg="green")
+        else:
+            pCam.config(bg="red")
+        if '7' in out:
+            pLong.config(bg="green")
+        else:
+            pLong.config(bg="red")
+    finally:
+        pedalFr.after(1000,pedalUpdates,PDS_q)
+

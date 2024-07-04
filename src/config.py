d811b2a02a719d7e917d2981344c434326f8b604
Keshara <keshara2032@gmail.com>
2024-07-04 11:11:55 -0400

    Config changes

--- src/config.py
+++ src/config.py
@@ -1,2 +1,17 @@
+#Threshold before a pedal is triggered
 PDS_ON_THRESHOLD = 2000
+
+#Capture port OBS virtual camera is on (Use src\Video\cam_test.py to test)
 OBS_CAPTURE_PORT = 3
+
+# Replace these values with your smartwatch's IP, port
+smartwatch_1_ip = '172.27.191.158'
+smartwatch_1_id = 'right'
+
+smartwatch_2_ip = '172.27.176.73'
+smartwatch_2_id = 'left'
+
+smartwatch_port = 7889
+
+#Camera type
+camera_type = "Intel"

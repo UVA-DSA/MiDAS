04540a77865ce74a0bc0a85c66d653b35ea6eeaa
Keshara <keshara2032@gmail.com>
2024-07-25 11:35:29 -0400

    config

--- src/config.py
+++ src/config.py
@@ -2,14 +2,14 @@
 PDS_ON_THRESHOLD = 3000
 PDS_LONG_THRESHOLD = 400
 
-PDS_PORT = "COM4"
+PDS_PORT = "COM3"
 
 #Capture port OBS virtual camera is on (Use src\Video\cam_test.py to test)
 OBS_CAPTURE_PORT = 5
 
 OBS_HOST = 'localhost'
 OBS_PORT = 4455
-OBS_WS_PASSWORD = "xZm5u8xiajqI0gLT"
+OBS_WS_PASSWORD = "zYScVXTTKppdI00d"
 
 # Replace these values with your smartwatch's IP, port
 smartwatch_1_ip = '192.168.0.104'
@@ -25,7 +25,7 @@ smartwatch_port = 7889
 #Camera type
 camera_type_1 = "Intel"
 camera_type_2 = "Zed"
-intel_exposure_value = 33000
+intel_exposure_value = 10000
 
 
 #general

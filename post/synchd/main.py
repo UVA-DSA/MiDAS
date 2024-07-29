87933003fc94a9f56858ffacef5c6a73033a5941
Keshara <keshara2032@gmail.com>
2024-07-29 15:00:37 -0400

    Main

--- post/synchd/main.py
+++ post/synchd/main.py
@@ -4,7 +4,9 @@ from OBSconvert import OBSConvert
 from PDSSync import PDSSync
 
 mainPath = 'post\data\*'
+finalPath = f"{mainPath}\Synched Data\final_sync.csv"
 OBSConvert(mainPath)
+
 PDSSync(mainPath)
 
 

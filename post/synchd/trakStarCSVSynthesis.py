2d7b10024f1588592755ee0e5c70c16e560ed7e2
vht2gm <vht2gm@virginia.edu>
2024-07-29 13:39:14 -0400

    fixed trackstar time units

--- post/synchd/trakStarCSVSynthesis.py
+++ post/synchd/trakStarCSVSynthesis.py
@@ -10,7 +10,9 @@ combined_file = 'data/PegT_S221_T1_2024-07-19/Trakstar/trakstar_organized.csv'
 
 df = pd.DataFrame(columns=['TrakStar time','SensorID_0','x_0','y_0','z_0','azimuth_0','elevation_0','roll_0','SensorID_1','x_1','y_1','z_1','azimuth_1','elevation_1','roll_1','SensorID_2','x_2','y_2','z_2','azimuth_2','elevation_2','roll_2','SensorID_3','x_3','y_3','z_3','azimuth_3','elevation_3','roll_3'])
 
-subset_trackstar = original_df['TrakStar Time'].iloc[::4].reset_index(drop=True)
+subset_trackstar = original_df['TrakStar Time'].iloc[::4].reset_index(drop=True) * 1000000000
+
+
 
 subset_0 = original_df[['SensorID','x','y','z','azimuth','elevation','roll']].iloc[::4].reset_index(drop=True)
 subset_1 = original_df[['SensorID','x','y','z','azimuth','elevation','roll']].iloc[1::4].reset_index(drop=True)

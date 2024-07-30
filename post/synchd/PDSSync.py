052af6bfda2a5a38584d95de96339c441450eafd
vht2gm <vht2gm@virginia.edu>
2024-07-30 11:20:03 -0400

    edited pdsSynch to fix concatenation issue

--- post/synchd/PDSSync.py
+++ post/synchd/PDSSync.py
@@ -21,24 +21,30 @@ def PDSSync(path):
     PDS_cols = pdsDF.columns.tolist()
     PDS_cols = [PDS_cols[-1]] + PDS_cols[:-1]
     PDS_cols.remove("Computer Time")
-    print(PDS_cols)
-    for col in PDS_cols:
-        finalDF[col] = pd.NA
+    PDS_matched = pd.DataFrame(columns=[PDS_cols])
+
+    # print(PDS_cols)
+    # for col in PDS_cols:
+        # finalDF[col] = pd.NA
 
     iterator = 0
     for i in obsStampList:
+        print(iterator)
         while True:
             try:
                 if i >= pdsStampList[iterator] and i < pdsStampList[iterator+1]:
-                    finalDF = pd.concat([finalDF, pdsDF.iloc[[iterator]]], ignore_index=True)
+                    PDS_matched = pd.concat([PDS_matched, pdsDF.iloc[[iterator]]], ignore_index=True)
                     break
             except Exception:
                 print("PDS Sync Error")
                 break
             iterator+=1
+
+    finalDF = pd.concat([finalDF, PDS_matched], axis = 1)
     finalDF.to_csv(f'{path}/Synched Data/PDS_sync.csv' , index=False)
 
 
 if __name__ == "__main__":
-    path = "./data/INGUINAL_S113_T3_2024-07-18"
+    path = "./data/PegT_S221_T1_2024-07-19"
     PDSSync(path)
+

5a80ea823c370791b7074e01256fde7922e9ebed
Hamid Roodabeh <hr.roodabeh@gmail.com>
2024-07-31 13:54:38 -0400

    working sw synch and resulting data

--- post/synchd/swSync.py
+++ post/synchd/swSync.py
@@ -1,4 +1,67 @@
-import pandas
+import pandas as pd
+import glob
 
-file_path = 'data/PegT_S221_T1_2024-07-19/smartwatch_data/sw_left/sw_data_acc.csv'
-obs_path
+
+obs_path = 'data/PegT_S221_T1_2024-07-19/Synched Data/sw_L_acc.csv'
+
+
+
+
+def swSync(path):
+
+
+    obsPath = f"{path}/obs/*.csv"
+    matchingPath = glob.glob(obsPath)
+    obsDF = pd.read_csv(matchingPath[0], index_col=False)
+    obsStampList = obsDF.iloc[:,0].tolist()
+
+
+    original_LA_DF = pd.read_csv(f'{path}/smartwatch_data/sw_left/sw_data_acc.csv')
+    original_LG_DF = pd.read_csv(f'{path}/smartwatch_data/sw_left/sw_data_gyro.csv')
+
+    original_RA_DF = pd.read_csv(f'{path}/smartwatch_data/sw_right/sw_data_acc.csv')
+    original_RG_DF = pd.read_csv(f'{path}/smartwatch_data/sw_right/sw_data_gyro.csv')
+
+    final_LA_DF = pd.read_csv(f'{path}/Synched Data/sw_L_acc.csv')
+    final_LG_DF = pd.read_csv(f'{path}/Synched Data/sw_L_gyro.csv')
+    
+    final_RA_DF = pd.read_csv(f'{path}/Synched Data/sw_R_acc.csv')
+    final_RG_DF = pd.read_csv(f'{path}/Synched Data/sw_R_gyro.csv')
+
+    files = [[original_LA_DF, final_LA_DF], [original_LG_DF, final_LG_DF], [original_RA_DF, final_RA_DF], [original_RG_DF, final_RG_DF]]
+    zero_row = pd.DataFrame([[0]*7], columns=original_LA_DF.columns)
+
+
+    for file in files:
+        iterator = 0
+        matched = pd.DataFrame()
+        print(file[0])
+        print(len(file[0]))
+        for i in obsStampList:
+            # if iterator % 1000 < 10:
+                # print(f"sw: {iterator}")
+            while iterator < len(file[0]) - 1:
+                try:
+                    if i >= file[0].iloc[:,6][iterator] and i < file[0].iloc[:,6][iterator+1]:
+                        matched = pd.concat([matched, file[0].iloc[[iterator]]], ignore_index=True)
+                        print("matching")
+                        break
+                    if i < file[0].iloc[:,6][iterator]:
+                        matched = pd.concat([matched, zero_row])
+                        break
+                except Exception as e:
+                    # print(f"swSync Error: {e}")
+                    break
+                iterator+=1
+
+        file[1] = pd.concat([file[1], matched], axis = 1)
+        
+    files[0][1].to_csv(f'{path}/Synched Data/sw_L_acc.csv', index =False)
+    files[1][1].to_csv(f'{path}/Synched Data/sw_L_gyro.csv', index = False)
+    files[2][1].to_csv(f'{path}/Synched Data/sw_R_acc.csv', index = False)
+    files[3][1].to_csv(f'{path}/Synched Data/sw_R_gyro.csv', index = False)
+
+
+if __name__ == "__main__":
+    path = "./data/PegT_S221_T1_2024-07-19"
+    swSync(path)

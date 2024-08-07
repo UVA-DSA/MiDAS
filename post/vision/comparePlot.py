56bafd698394d29dee9cf778b82eef2a3c0a0775
Keshara <keshara2032@gmail.com>
2024-08-07 15:09:07 -0400

    Plotting pedalDetection

---
+++ post/vision/comparePlot.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import glob
+import os
+
+PDSSync = pd.DataFrame()
+pedalDetection = pd.DataFrame()
+
+def plot(path):
+    PDSSync = pd.read_csv(f"{path}/Synched Data/PDS_sync.csv")
+    pedalDetection = pd.read_csv(f"{path}/obs/pedal_detection.csv")
+
+    figure, axis = plt.subplots(2, 1, figsize=(20, 10))
+
+    axis[0].plot(pedalDetection.index, pedalDetection['Upper Left'], color = "red", label = "Upper Left Pedal")
+    axis[0].plot(pedalDetection.index, pedalDetection['Upper Right'], color = "yellow", label = "Upper Right Pedal")
+    axis[0].plot(pedalDetection.index, pedalDetection['Lower Left'], color = "green", label = "Lower Left Pedal")
+    axis[0].plot(pedalDetection.index, pedalDetection['Lower Right'], color = "blue", label = "Lower Right Pedal")
+
+    axis[0].set_title('Pedal Indicators vs Frame')
+    axis[0].set_ylim(0,1.5)
+    axis[0].set_xlabel("Frame #")
+    axis[0].legend()
+
+    axis[1].scatter(PDSSync.index, PDSSync['Pedal 1 Pressure'], color = "red", label = "Upper Left Pedal",s=3)
+    axis[1].scatter(PDSSync.index, PDSSync['Pedal 2 Pressure'], color = "yellow", label = "Upper Right Pedal",s=3)
+    axis[1].scatter(PDSSync.index, PDSSync['Pedal 3 Pressure'], color = "green", label = "Lower Left Pedal",s=3)
+    axis[1].scatter(PDSSync.index, PDSSync['Pedal 4 Pressure'], color = "blue", label = "Lower Right Pedal",s=3)
+
+    axis[1].set_title('Synced Pedal Data vs Frame')
+    axis[1].set_ylim(0,10000)
+    axis[1].set_xlabel("Frame #")
+    axis[1].legend()
+
+    plt.savefig(f'{path}/plots/pedal_detection_comp.png',dpi=300, bbox_inches='tight', format='png')
+    plt.close()
+
+
+if __name__ == '__main__':
+    matchingPath = glob.glob("./post/data/B*")
+    for path in matchingPath:
+        print(path)
+        PDSSync = pd.read_csv(f"{path}/Synched Data/PDS_sync.csv")
+        detectionPath = glob.glob(f"{path}/obs/2024*.csv")
+        pedalDetection = pd.read_csv(detectionPath[0])
+        try:
+            os.mkdir(f"{path}/plots/")
+            print(f"Directory created successfully.")
+        except FileExistsError:
+            print(f"Directory already exists.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        plot(path)
+        
--- post/vision/pedalDetection.py
+++ post/vision/pedalDetection.py
@@ -1,9 +1,9 @@
 import numpy as np
 import cv2 as cv
 import csv
+import glob
+import comparePlot
 
-path = "post/data/Bowel_S216_T1_2024-07-18/obs/2024-07-18 15-48-29"
-frameNum = 0
 onePixel = (600,1020)
 twoPixel = (725,1020)
 threePixel = (1230,1020)
@@ -11,50 +11,59 @@ fourPixel = (1580,1020)
 threshold = 150
 
 if __name__ == "__main__":
-    cap = cv.VideoCapture(f"{path}.mkv")
-    cap.set(cv.CAP_PROP_POS_FRAMES, frameNum)
-    cv.namedWindow('frame',cv.WINDOW_NORMAL)
-    csvFile = open(f"{path}.csv", 'w', newline='')
-    out = csv.writer(csvFile)
-    out.writerow(["Frame Num","Upper Left", "Lower Left", "Upper Right", "Lower Right"])
+    matchingPath = glob.glob("D:/Data MIDAS/*")
+    matchingPath.pop(0)
+    for path in matchingPath:
+        frameNum = 0
+        vidpath = glob.glob(f"{path}/obs/*.mkv")
+        cap = cv.VideoCapture(vidpath[0])
+        print(vidpath[0])
+        cap.set(cv.CAP_PROP_POS_FRAMES, frameNum)
+        csvFile = open(f"{path}/obs/pedal_detection.csv", 'w', newline='')
+        out = csv.writer(csvFile)
+        out.writerow(["Frame Num","Upper Left", "Lower Left", "Upper Right", "Lower Right"])
 
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        # cv.imshow("frame",frame)
-        frameNum += 1
-        oneColor = frame[onePixel[1],onePixel[0]]
-        twoColor = frame[twoPixel[1],twoPixel[0]]
-        threeColor = frame[threePixel[1], threePixel[0]]
-        fourColor = frame[fourPixel[1],fourPixel[0]]
-        # print(threeColor,fourColor)
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            # cv.imshow("frame",frame)
+            frameNum += 1
+            if frameNum % 5000 == 0:
+                print(frameNum)
+            oneColor = frame[onePixel[1],onePixel[0]]
+            twoColor = frame[twoPixel[1],twoPixel[0]]
+            threeColor = frame[threePixel[1], threePixel[0]]
+            fourColor = frame[fourPixel[1],fourPixel[0]]
+            # print(threeColor,fourColor)
 
-        if (int(oneColor[1]) + int(oneColor[2]))/2 > threshold:
-            cv.imwrite(f'{path}/img{frameNum}.jpg', frame)
-            upperLeft = 1
-        else:
-            upperLeft = 0
-        if int(oneColor[0]) > threshold:
-            cv.imwrite(f'{path}/img{frameNum}.jpg', frame)
-            lowerLeft = 1
-        else:
-            lowerLeft = 0
+            if (int(oneColor[1]) + int(oneColor[2]))/2 > threshold:
+                # cv.imwrite(f'{path}/img{frameNum}.jpg', frame)
+                upperLeft = 1
+            else:
+                upperLeft = 0
+            if int(oneColor[0]) > threshold:
+                # cv.imwrite(f'{path}/img{frameNum}.jpg', frame)
+                lowerLeft = 1
+            else:
+                lowerLeft = 0
 
-        if (int(fourColor[1]) + int(fourColor[2]))/2 > threshold or (int(threeColor[1]) + int(threeColor[2]))/2 > threshold:
-            cv.imwrite(f'{path}/img{frameNum}.jpg', frame)
-            upperRight = 1
-        else:
-            upperRight = 0
-        if int(fourColor[0]) > threshold or int(threeColor[0]) > threshold:
-            cv.imwrite(f'{path}/img{frameNum}.jpg', frame)
-            lowerRight = 1
-        else:
-            lowerRight = 0
+            if (int(fourColor[1]) + int(fourColor[2]))/2 > threshold or (int(threeColor[1]) + int(threeColor[2]))/2 > threshold:
+                # cv.imwrite(f'{path}/img{frameNum}.jpg', frame)
+                upperRight = 1
+            else:
+                upperRight = 0
+            if int(fourColor[0]) > threshold or int(threeColor[0]) > threshold:
+                # cv.imwrite(f'{path}/img{frameNum}.jpg', frame)
+                lowerRight = 1
+            else:
+                lowerRight = 0
 
-        out.writerow([frameNum,upperLeft, lowerLeft, upperRight, lowerRight])
-        # prevColor = rightColor
-        if cv.waitKey(1) & 0xFF == ord('q'): 
-            break
-    print("Done")
-    cap.release()
+            out.writerow([frameNum,upperLeft, lowerLeft, upperRight, lowerRight])
+            csvFile.flush()
+            if cv.waitKey(1) & 0xFF == ord('q'): 
+                break
+        print("Saved csv")
+        cap.release()
+        comparePlot.plot(path)
+        print("Saved plot")
